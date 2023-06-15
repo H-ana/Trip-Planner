177c2b1886934c54ca6ec16c48b84fee1db30fa2
Arjun J <arjunjins2001@gmail.com>
2023-06-15 15:16:31 +0530

    added conditional rendering

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,32 +1,40 @@
 import "./home.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Home = () => {
-const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState("");
 
-const handleInputChange = (event) => {
+  const handleInputChange = (event) => {
     setInputText(event.target.value);
-};
+  };
 
-    const handleRunClick = () => {
+  const handleRunClick = () => {
     console.log(`Run clicked with input: ${inputText}`);
-};
-
-return (
-    <div className="home" >
-        <div className="home_title">VacaPlanner</div>
-        <div className="home_tagline">Travel smarter, not harder!</div>
-        <div className="input-container">
-        <input className="startinput"
-        type="text"
-        value={inputText}
-        onChange={handleInputChange}
-        placeholder="Start typing here.."
+  };
+  let itinerary = ""
+  return (
+    <>
+    <div className="home">
+      <div className="home_title">VacaPlanner</div>
+      <div className="home_tagline">Travel smarter, not harder!</div>
+      <div className="input-container">
+        <input
+          className="startinput"
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder="Start typing here"
         />
         <button onClick={handleRunClick}>Search</button>
+      </div>
     </div>
-    </div>
-);
+      {itinerary == "" ? (<></>) : (<div className="container">
+        <div className="content">
+          {itinerary}
+        </div>
+      </div>)}
+      </>
+  );
 };
 
 export default Home;
